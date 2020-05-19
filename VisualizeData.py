fd9886da155c0d7d820fd73d40e388b9c36a8e3e
JonatanHjelm95 <cph-jh409@cphbusiness.dk>
2020-05-19 15:06:38 +0200

    matplotlib modified

--- VisualizeData.py
+++ VisualizeData.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt, mpld3
 import dataHandler
 import numpy as np
+import pandas as pd
 from datetime import datetime, timedelta
 import time
 import symbolLookup
@@ -12,31 +13,46 @@ def convertToDate(ts):
 
 def create_graph(name, _type):
     latestData, predictionData, confidence, method, title = dataHandler.getPriceData(_type, name.lower())
-    # Converting list of dicts in to 2 seperate lists *2
-    latestDates, latestPrices = [list(col) for col in zip(*[d.values() for d in latestData])]
-    predictionDates, predictionPrices = [list(col) for col in zip(*[p.values() for p in predictionData])]
     
-    for p in predictionPrices:
-        latestPrices.append(p)
-    for d in predictionDates:
-        latestDates.append(d)
-    
-    dates  = []
-    for l in latestDates:
-        dates.append(convertToDate(l))
-
-    x = dates
-    xi = list(range(len(x)))
-    plt.figure(figsize=(20,5))
-    plt.xticks(xi, x)
-    plt.plot(latestPrices, 'ks-')
-    plt.tight_layout()
+    # Creating 1 dataframe from 2 lists of dicts
+    df = pd.DataFrame(merge_lists_of_dicts(latestData, predictionData))
+    # setting latest timestamp from the actual price data
+    currentTimestamp = latestData[-1]['timestamp']
+    # defining plotsize
+    plt.subplots(figsize=(20,5))
+    # plotting all data
+    plt.plot(df['date'], df['price'], 'ks-', c = 'blue', label='Historical prices')
+    # plotting prediction data on top
+    plt.plot(df[df.timestamp >= currentTimestamp]['date'], df[df.timestamp >= currentTimestamp]['price'], 'ks-', c = 'red', label='Predicted prices')
+    # Setting title, methods and Coef
     plt.title(createTitle(_type, title, name)+ ', ' + str(method) +', Confidence: '+ str(round(float(confidence),2)))
-    plt.setp(plt.xticks()[1], rotation=30)
+    # Rotating x labels
+    plt.xticks(rotation = 45)
+    plt.ylabel('USD')
+    plt.savefig('figure')
+    #plt.show()
+    return df.to_json()
+    #mpld3.save_json(fig=fig, fileobj='test')
+    #return mpld3.fig_to_html(plt.figure())
+
     
-    #plt.plot(predictionPrices)
-    mpld3.show()
     
+
+def merge_lists_of_dicts(l1, l2):
+    wholeData = []
+    for l in l1:
+        data = {}
+        data['timestamp'] = l['timestamp']
+        data['price'] = l['price']
+        data['date'] = createDate(l['timestamp'])
+        wholeData.append(data)
+    for l in l2:
+        data = {}
+        data['timestamp'] = l['timestamp']
+        data['price'] = l['price']
+        data['date'] = createDate(l['timestamp'])
+        wholeData.append(data)
+    return wholeData
 
 def createTitle(_type, symbol, name):
     if _type == 'STOCK':
@@ -44,8 +60,15 @@ def createTitle(_type, symbol, name):
     if _type == 'FOREX':
         return symbolLookup.forex_getNameFromSymbol(symbol)
     if _type == 'CRYPTO':
-        return symbol
+        return symbol.capitalize()
+
+def createDate(timestamp):
+    months = {1:'jan', 2:'feb', 3:'mar', 4:'apr', 5:'may', 6:'jun', 7:'jul', 8:'aug', 9:'sep', 10:'oct', 11:'nov', 12:'dec'}
+    date = datetime.fromtimestamp(timestamp)
+    return months[date.month].capitalize() + ' ' + str(date.day)
 
 if __name__ == "__main__":
-    create_graph('carlsberg', 'STOCK')
+    #print(createDate(1586124000))
+    print(create_graph('bitcoin', 'CRYPTO'))
+    #print(json)
     #print(convertToDate(1589061600))
